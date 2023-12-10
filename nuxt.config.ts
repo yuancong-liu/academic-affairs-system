ec10d75b6e07b061520187f7be0526702ce1ec38
YUAN-TSUNG <satoshi.liu@icloud.com>
2023-12-10 19:02:26 +0900

    📕 storybook integrated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@storybook-vue/nuxt-storybook"],
+  storybook: {
+    url: "http://localhost:6006",
+    storybookRoute: "/__storybook__",
+    port: 6006,
+  },
+});
