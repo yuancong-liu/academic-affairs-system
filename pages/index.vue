42d19c25a2b399f555ef576d1dfc40fc66bddcb4
YUAN-TSUNG <satoshi.liu@icloud.com>
2024-03-07 23:45:18 +0900

    ✨ home page layout added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <NuxtLayout :title="title">
-    <div>
-      <p>This is home page</p>
+    <div class="grid w-full grid-cols-1 gap-2 md:grid-cols-2 md:grid-rows-2">
+      <div class="row-span-2 w-full rounded bg-white p-2">AREA 1</div>
+      <div class="w-full rounded bg-white p-2">AREA 2</div>
+      <div class="w-full rounded bg-white p-2">AREA 3</div>
     </div>
   </NuxtLayout>
 </template>
